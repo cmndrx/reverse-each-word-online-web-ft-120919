0c2db7b06fa3dca440cfe7cd35926084d5a5b1dc
Autonyo Sanders <tonio.sanders@gmail.com>
2019-12-11 22:47:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(words)
   array = words.split(" ")
   new_array = []
-  new_array = words.collect {|word| word.reverse}
+  new_array = array.collect {|word| word.reverse}
   new_array.join(" ")
 end  
